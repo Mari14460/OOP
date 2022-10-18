b92c4f8eae0e2193019796a85495348f05649a41
Marina Zinchenko <amanda79116@gmail.com>
2022-10-17 21:54:19 -0500

    first commit

---
+++ src/main/java/ru/netology/OopPrinciples/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology.OopPrinciples;
+
+public class FormDate {
+    int day;
+    int month;
+    int year;
+}
---
+++ src/main/java/ru/netology/OopPrinciples/Post.java
@@ -0,0 +1,27 @@
+package ru.netology.OopPrinciples;
+
+public class Post {
+    /*
+    {
+      "birthday": {
+        "day": 13,
+        "month": 6,
+        "year": 1999
+      },
+      "name": "Иван",
+      "passport": "4444 № 44444444",
+      "patronymic": "Иванович",
+      "phone": "+7 (999)-999-99-99",
+      "surname": "Иванов",
+      "subscription": true
+    }
+    */
+
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
